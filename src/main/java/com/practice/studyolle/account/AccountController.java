c6ed419906dd57843fc0487debb7977864fcab92
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-03-03 21:43:50 +0900

    #3 회원가입 뷰 구현( 타임리프 적용 및 403 에러 해결)

--- src/main/java/com/practice/studyolle/account/AccountController.java
+++ src/main/java/com/practice/studyolle/account/AccountController.java
@@ -9,6 +9,11 @@ public class AccountController {
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
+
+        // 1. 원래는 아래
+        // model.addAttribute("signUpForm", new SignUpForm());
+        // 2. 클래스의 이름이 키로 사용할 수 있음. 생략 가능
+        model.addAttribute(new SignUpForm());
         return "account/sign-up";
     }
 }
--- src/main/java/com/practice/studyolle/config/SecurityConfig.java
+++ src/main/java/com/practice/studyolle/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.practice.studyolle.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,10 +19,19 @@ public class SecurityConfig {
         http
                 .authorizeRequests()
                 .mvcMatchers("/", "/login", "/sign-up", "/check-email", "/check-email-token",
-                        "/email-login","/check-email-login", "/login-link").permitAll()
+                        "/email-login", "/check-email-login", "/login-link").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
                 .anyRequest().authenticated();
 
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web ->
+                web.ignoring()
+                        .mvcMatchers("/node_modules/**")
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+        );
     }
 }
