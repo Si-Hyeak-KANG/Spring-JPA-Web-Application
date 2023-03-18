8002cd569d51987739e9404175f10fe8764f2a8e
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-03-18 17:10:53 +0900

    #23 관심 주제 태그 조회 기능 구현

--- src/main/java/com/practice/studyolle/account/AccountService.java
+++ src/main/java/com/practice/studyolle/account/AccountService.java
@@ -21,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -137,5 +138,10 @@ public class AccountService implements UserDetailsService {
         byId.ifPresent(a -> a.getTags().add(tag));
 
         // accountRepository.getOne() -> 지연(Lazy) 로딩
+    }
+
+    public Set<Tag> getTags(Account account) {
+        Optional<Account> byId = accountRepository.findById(account.getId());
+        return byId.orElseThrow().getTags();
     }
 }
