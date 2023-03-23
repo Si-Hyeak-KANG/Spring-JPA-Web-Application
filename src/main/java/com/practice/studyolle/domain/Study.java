7fc590775ef0609cd04c309ffaa0c1b602a90614
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-03-24 08:01:13 +0900

    #30 Study 도메인 생성

---
+++ src/main/java/com/practice/studyolle/domain/Study.java
@@ -0,0 +1,53 @@
+package com.practice.studyolle.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder @EqualsAndHashCode(of = "id")
+public class Study {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany
+    private Set<Account> managers = new HashSet<>();
+
+    @ManyToMany
+    private Set<Account> members = new HashSet<>();
+
+    @Column(unique = true)
+    private String path;
+
+    private String title;
+
+    private String shortDescription;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String fullDescription;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String image;
+
+    @ManyToMany
+    private Set<Tag> tags;
+
+    private LocalDateTime publishedDateTime;
+
+    private LocalDateTime closeDateTime;
+
+    private LocalDateTime recruitingUpdateTime;
+
+    private boolean recruiting;
+
+    private boolean closed;
+
+    private boolean useBanner;
+
+}
