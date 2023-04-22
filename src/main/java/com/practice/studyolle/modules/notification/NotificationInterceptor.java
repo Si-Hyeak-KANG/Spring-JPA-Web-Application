98a0b0977f5ebbf70fe0b46aec74f59036d1a75e
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-04-22 12:02:12 +0900

    #59 알림 리스트 조회 뷰 및 기능 구현

--- src/main/java/com/practice/studyolle/modules/notification/NotificationInterceptor.java
+++ src/main/java/com/practice/studyolle/modules/notification/NotificationInterceptor.java
@@ -5,7 +5,6 @@ import com.practice.studyolle.modules.account.UserAccount;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
--- src/main/java/com/practice/studyolle/modules/notification/NotificationRepository.java
+++ src/main/java/com/practice/studyolle/modules/notification/NotificationRepository.java
@@ -2,8 +2,18 @@ package com.practice.studyolle.modules.notification;
 
 import com.practice.studyolle.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Transactional(readOnly = true)
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     long countByAccountAndChecked(Account account, boolean checked);
+
+    @Transactional
+    List<Notification> findByAccountAndCheckedOrderByCreatedDateTimeDesc(Account account, boolean checked);
+
+    @Transactional
+    void deleteByAccountAndChecked(Account account, boolean checked);
 }
