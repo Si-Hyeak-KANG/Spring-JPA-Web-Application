0fb19c554cd8aeec7e4ca3502cf35ff8a626b69b
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-04-23 23:21:22 +0900

    #66 RuntimeException 처리 및 error 페이지 구현

---
+++ src/main/java/com/practice/studyolle/modules/main/ExceptionAdvice.java
@@ -0,0 +1,26 @@
+package com.practice.studyolle.modules.main;
+
+import com.practice.studyolle.modules.account.Account;
+import com.practice.studyolle.modules.account.CurrentAccount;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@ControllerAdvice
+public class ExceptionAdvice {
+
+    @ExceptionHandler
+    public String handleRuntimeException(@CurrentAccount Account account, HttpServletRequest request, RuntimeException e) {
+        if (account != null) {
+            log.info("'{}' requested '{}'", account.getNickname(), request.getRequestURI() );
+        } else {
+            log.info("requested '{}'", request.getRequestURI());
+        }
+
+        log.error("bad request", e);
+        return "error";
+    }
+}
